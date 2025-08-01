9eda49167345f5354f4315ce2e5c669dbe4ad123
industrial-demon <drew.naumenko@gmail.com>
2025-08-02 00:02:27 +0300

    added models: country, state, city, department, empoloyee

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Livewire\Livewire;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
@@ -18,5 +18,15 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
+
+
+Livewire::setScriptRoute(function ($handle) {
+return Route::get('/laravel-store/public/livewire/livewire.js', $handle);
+});
+
+Livewire::setUpdateRoute(function ($handle) {
+return Route::post('/laravel-store/public/livewire/update', $handle);
+});
+
 
 require __DIR__.'/auth.php';
